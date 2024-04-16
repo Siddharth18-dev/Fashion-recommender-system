c64e15c9df80776880e6273934abc7b11b66d02d
Siddharth Shravan Jha <65068933+Siddharth18-dev@users.noreply.github.com>
2024-04-16 18:05:22 +0530

    updated main.py file and also added prediction Mod

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+import tensorflow
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from numpy.linalg import norm
+import os
+from tqdm import tqdm
+import pickle
 
+model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
+
+
+def extract_feature(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img_array)
+    result = model.predict(preprocessed_img).flatten()
+    normalised_result = result / norm(result)
+
+    return normalised_result
+
+
+filenames = []
+
+for file in os.listdir('images'):
+    filenames.append(os.path.join('images', file))
+
+feature_list = []
+
+for file in tqdm(filenames):
+    feature_list.append(extract_feature(file, model))
+
+pickle.dump(feature_list, open('embeddings.pkl', 'wb'))
+pickle.dump(filenames, open('filenames.pkl', 'wb'))
